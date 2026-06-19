7c917dad4e77563ea696f7e5077351ea29ef8607
Ahmed Atia <engahmedatia15@gmail.com>
2026-06-19 20:47:01 +0300

    10

--- routes/paymob.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPaymentLink } from "../paymob/paymob.controller.js";
-
-const router = express.Router();
-
-router.post("/pay", createPaymentLink);
-
-export default router;
-
